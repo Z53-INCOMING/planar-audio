fcb65b67058154a59d6ef65ee59d47757ea0fde8
Tessimal <75651517+Z53-INCOMING@users.noreply.github.com>
2026-06-24 15:35:25 -0700

    any length of song

--- planar_audio.gd
+++ planar_audio.gd
@@ -5,21 +5,37 @@ class_name PlanarAudio extends Node2D
 func _ready():
 	var sound_file = FileAccess.open("res://song.raw", FileAccess.WRITE)
 	
-	var song_image := song.get_image()
-	var seconds_in_a_pixel := 0.25
-	
-	var angle := TAU / 8.0
-	
+	# where we're slicing
+	var angle := 0.0
 	var offset := Vector2.ZERO
 	
-	for i in 44100:
+	# song
+	var song_image := song.get_image()
+	var seconds_in_a_pixel := 0.25
+	var ray_start := Vector3(offset.x, offset.y, 0.5)
+	var ray_dir := Vector3(cos(angle), sin(angle), 0.0)
+	var diagonal := song.get_size().length()
+	var intersection = AABB(Vector3.ZERO, Vector3(song.get_width(), song.get_height(), 1.0)).intersects_ray(ray_start + (ray_dir * diagonal * 2.0), -ray_dir)
+	if !intersection:
+		print("what!!!")
+		return
+	var length := ray_start.distance_to(intersection)
+	
+	print(length)
+	
+	for i in 44100 * length * seconds_in_a_pixel:
 		var time := float(i) / 44100.0
 		
-		var magnitude := song_image.get_pixel(
-			int((time / seconds_in_a_pixel) + offset.x) * cos(angle),
-			int((time / seconds_in_a_pixel) + offset.y) * sin(angle)
-		).r
+		var color := song_image.get_pixel(
+			int(floor((time / seconds_in_a_pixel) + offset.x)) * cos(angle),
+			int(floor((time / seconds_in_a_pixel) + offset.y)) * sin(angle)
+		)
 		
-		sound_file.store_16(sin(time * TAU * 720.0) * magnitude * 4096.0)
+		var frequency := pow(2.0, color.h * 4.0) * 64.0
+		
+		sound_file.store_16(compute_tone(time, frequency, color.v))
 	
 	sound_file.close()
+
+func compute_tone(time: float, frequency: float, amplitude: float) -> int:
+	return sin(time * TAU * frequency) * amplitude * 4096.0
